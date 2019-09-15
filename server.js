fd07ce538062e686157481c03e20532a794358f2
Tristen Spruill <Tspruil6@uncc.edu>
2019-09-14 23:31:28 -0400

    initial

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ dotenv.config({path: './config.env'});
 
 // Profile routes 
 app.use('/api/v1/profile',require('./routes/profiles.js'))
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static(__dirname + '/public/'));
+  
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  }
 const port = process.env.PORT || 5000
 app.listen(port,()=>{
     console.log(`App listening on port ${port}!`)
